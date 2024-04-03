734fa3095f17f5a5c9963295b67445db26551928
InteractiveOfCelcius <134863563+InteractiveOfCelcius@users.noreply.github.com>
2024-04-03 14:22:22 +0200

    Update kat.py

--- kat.py
+++ kat.py
@@ -2,11 +2,16 @@
 KAT Package library made by Interactive Of Celcius and handled by them!
 """
 
-def katCommandExecute(args):
+_KATVER = float('24.42')
+
+def KAT(args):
     if args:
         mtod = requests.get('https://interactiveofcelcius.github.io/KAT/mtod')
-        if mtod.status_code == 200:
+        ver = requests.get('https://interactiveofcelcius.github.io/KAT/ver.txt')
+        if mtod.status_code and ver.status_code == 200:
             print(Fore.CYAN + mtod.text)
+            if float(ver.text) != _KATVER:
+                warn(f'You are using an outdated version ({ver.text}) of KAT, please update it with "kat update kat"')
         else:
             warn("MTOD Couldn't be loaded. Please verify if the server is on!")
         if args[0] == 'install':
@@ -26,6 +31,38 @@ def katCommandExecute(args):
                 succes(f"Package '{args[1]}' downloaded and ready to use.")
             else:
                 warn(f'package: {args[1]} does not exist.')
+        if args[0] == 'update':
+            package_name = args[1]
+            package_url = f'https://interactiveofcelcius.github.io/KAT/{package_name}.py'
+            package_file_path = os.path.join(folder_path, f"{package_name}.py")
+            if os.path.exists(package_file_path):
+                print(f"Updating package {package_name}...")
+                os.remove(package_file_path)
+            print(f"Downloading package {package_name}...")
+            response = requests.get(package_url)
+            if response.status_code == 200:
+                succes(f"Package {package_name} found.")
+                with open(package_file_path, 'w') as package_file:
+                    package_file.write(response.text)
+                succes(f"Package '{package_name}' downloaded and updated successfully.")
+                warn('''=================WARNING===================
+Please restart KATLine to get the updated packages!
+===========================================
+                     ''')
+            else:
+                warn(f"Package '{package_name}' does not exist.")
+        if args[0] == 'uninstall':
+            package_name = args[1]
+            if package_name != 'kat':
+                package_file_path = os.path.join(folder_path, f"{package_name}.py")
+                if os.path.exists(package_file_path):
+                    print(f"Removing package {package_name}...")
+                    os.remove(package_file_path)
+                    succes("Package removed. To get the commands removed please reload KATLine")
+            else:
+                warn("You can't uninstall KAT")
+
+            
     else:
         print(f'KAT is a command used to install packages for KATLine.')
 
@@ -37,5 +74,5 @@ def doQuit(args):
     
     
 # Register all the commands
-registerCommand('kat', katCommandExecute)
+registerCommand('kat', KAT)
 registerCommand('quit', doQuit)

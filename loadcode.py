1e98d653d271e4d4222644765ea2e7c2a9387ce1
InteractiveOfCelcius <134863563+InteractiveOfCelcius@users.noreply.github.com>
2024-04-03 20:22:42 +0200

    Create loadcode.py

---
+++ loadcode.py
@@ -0,0 +1,29 @@
+
+"""
+Made by PSVKS. This code only works on KATLine.
+Last update: 03/04/2024
+"""
+
+
+_DATA = ["LoadCodeAPI", "psvks"]
+
+
+class LoadCode:
+  def __init__(self) -> None:
+    pass
+
+  def Load(self, url):
+    r = requests.get(url)
+    if r.status_code == 200:
+      try:
+        exec(r.text)
+      except Exception as e:
+        warn(f'LoadCode API Error: {e}')
+
+
+def package():
+  def LoadCodeAPI_Verify(args):
+    success('LoadCode API is loaded! Made by PSVKS')
+  
+  
+  registerCommand('loadcode', LoadCodeAPI_Verify)
